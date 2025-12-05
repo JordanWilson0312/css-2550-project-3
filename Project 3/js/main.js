4cb66a72fde9668eb4ab9b163058efc21b36a5ec
JordanWilson0312 <jaime.wilson0312@gmail.com>
2025-12-05 16:45:56 -0700

    Delete Project 3 directory

--- Project 3/js/main.js
+++
@@ -1,5 +0,0 @@
-function showSection(id) {
-  const sections = document.querySelectorAll("main section");
-  sections.forEach(s => s.style.display = "none");
-  document.getElementById(id).style.display = "block";
-}
